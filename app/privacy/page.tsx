43f8b732b2a8abab8bb759c2f22267d76e4c9fbc
TrustfulDev <sssstevenvu@gmail.com>
2024-04-29 22:40:28 -0700

    Styling fixes for Privacy Page

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 
 export default async function Privacy() {
-        
         return (
-                <div className="max-w-2xl mx-auto p-4">
+                <div className="mx-auto p-4 lg:overflow-y-auto m-4 md:mb-6 md:mx-0 mt-0">
                     <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
                     <p className="text-sm mb-2">Last updated April 29, 2024</p>
                     <p className="mb-4">This privacy notice for Dena Energy Inc ("we," "us," or "our"), describes how and why we might collect, store, use, and/or share ("process") your information when you use our services ("Services"), such as when you:</p>
@@ -11,7 +10,7 @@ export default async function Privacy() {
                         <li>Visit our website at <a href="https://energydian.com" className="text-blue-500">https://energydian.com</a>, or any website of ours that links to this privacy notice</li>
                         <li>Engage with us in other related ways, including any sales, marketing, or events</li>
                     </ul>
-                    <p className="mb-4">Questions or concerns? Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices, please do not use our Services. If you still have any questions or concerns, please contact us at <a href="mailto:privacy@dena.energy" className="text-blue-500">privacy@dena.energy</a>.</p>
+                    <p className="mb-4">Questions or concerns? Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices, please do not use our Services. If you still have any questions or concerns, please contact us at <a href="mailto:info@dena.energy" className="text-blue-500">info@dena.energy</a>.</p>
                     
                     {/* Summary of Key Points */}
                     <div className="border-t-2 border-gray-300 pt-4 mt-4">
@@ -31,7 +30,7 @@ export default async function Privacy() {
                     </div>
 
                     {/* Table Of Contents */}
-                    <div className="border-t-2 border-gray-300 pt-4 mt-4">
+                    <div className="border-t-2 border-gray-300 pt-4 mt-4" id="table-of-contents">
                         <h2 className="text-lg font-bold mb-2">Table Of Contents</h2>
                         
                         <ul className="list pl-6 mb-4">
@@ -53,7 +52,7 @@ export default async function Privacy() {
                     </div>
         
                     {/* What Information Do We Collect? */}
-                    <div className="border-t-2 border-gray-300 pt-4 mt-4">
+                    <div className="border-t-2 border-gray-300 pt-4 mt-4" id="personal-information">
                         <h2 className="text-lg font-bold mb-2">1. What Information Do We Collect?</h2>
                         <p><strong>Personal information you disclose to us</strong></p>
                         <p className="mb-4">In Short: We collect personal information that you provide to us.</p>
@@ -72,8 +71,8 @@ export default async function Privacy() {
                             <li><strong>Location Data.</strong> We collect location data such as information about your device's location, which can be either precise or imprecise. How much information we collect depends on the type and settings of the device you use to access the Services. For example, we may use GPS and other technologies to collect geolocation data that tells us your current location (based on your IP address). You can opt out of allowing us to collect this information either by refusing access to the information or by disabling your Location setting on your device. However, if you choose to opt out, you may not be able to use certain aspects of the Services.</li>
                         </ul>
                     </div>
-                        {/* How Do We Process Your Information? */}
-                    <div className="border-t-2 border-gray-300 pt-4 mt-4">
+                {/* How Do We Process Your Information? */}
+                <div className="border-t-2 border-gray-300 pt-4 mt-4">
                         <h2 className="text-lg font-bold mb-2">2. How Do We Process Your Information?</h2>
                         <p><strong>In Short:</strong> We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent.</p>
                         <p>We process your personal information for a variety of reasons, depending on how you interact with our Services, including:</p>
@@ -87,9 +86,10 @@ export default async function Privacy() {
                         <li><strong>To determine the effectiveness of our marketing and promotional campaigns.</strong> We may process your information to better understand how to provide marketing and promotional campaigns that are most relevant to you.</li>
                         <li><strong>To comply with our legal obligations.</strong> We may process your information to comply with our legal obligations, respond to legal requests, and exercise, establish, or defend our legal rights.</li>
                         </ul>
-                   </div>
-                   {/* When and With Whom Do We Share Your Personal Information? */}
-               <div className="border-t-2 border-gray-300 pt-4 mt-4">
+                </div>
+
+                {/* When and With Whom Do We Share Your Personal Information? */}
+                <div className="border-t-2 border-gray-300 pt-4 mt-4">
                         <h2 className="text-lg font-bold mb-2">3. When and With Whom Do We Share Your Personal Information?</h2>
                         <p><strong>In Short:</strong> We may share information in specific situations described in this section and/or with the following third parties.</p>
                         <p>We may need to share your personal information in the following situations:</p>
@@ -97,40 +97,44 @@ export default async function Privacy() {
                         <li><strong>Business Transfers.</strong> We may share or transfer your information in connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to another company.</li>
                         <li><strong>When we use Google Analytics.</strong> We may share your information with Google Analytics to track and analyze the use of the Services. To opt out of being tracked by Google Analytics across the Services, visit <a href="https://tools.google.com/dlpage/gaoptout" className="text-blue-500">https://tools.google.com/dlpage/gaoptout</a>. For more information on the privacy practices of Google, please visit the Google Privacy & Terms page.</li>
                         </ul>
-                   </div>
+                </div>
+
                 {/* Do We Use Cookies and Other Tracking Technologies? */}
                 <div className="border-t-2 border-gray-300 pt-4 mt-4">
                         <h2 className="text-lg font-bold mb-2">4. Do We Use Cookies and Other Tracking Technologies?</h2>
                         <p><strong>In Short:</strong> We may use cookies and other tracking technologies to collect and store your information.</p>
                         <p>We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store information. Specific information about how we use such technologies and how you can refuse certain cookies is set out in our Cookie Notice.</p>
-                    </div>
+                </div>
+
                 {/* How Do We Handle Your Social Logins? */}
                 <div className="border-t-2 border-gray-300 pt-4 mt-4">
                         <h2 className="text-lg font-bold mb-2">5. How Do We Handle Your Social Logins?</h2>
                         <p><strong>In Short:</strong> If you choose to register or log in to our Services using a social media account, we may have access to certain information about you.</p>
                         <p>Our Services offer you the ability to register and log in using your third-party social media account details (like your Facebook or Twitter logins). Where you choose to do this, we will receive certain profile information about you from your social media provider. The profile information we receive may vary depending on the social media provider concerned, but will often include your name, email address, friends list, and profile picture, as well as other information you choose to make public on such a social media platform.</p>
                         <p>We will use the information we receive only for the purposes that are described in this privacy notice or that are otherwise made clear to you on the relevant Services. Please note that we do not control, and are not responsible for, other uses of your personal information by your third-party social media provider. We recommend that you review their privacy notice to understand how they collect, use, and share your personal information, and how you can set your privacy preferences on their sites and apps.</p>
-                    </div>
+                </div>
+
                 {/* How Long Do We Keep Your Information? */}
                 <div className="border-t-2 border-gray-300 pt-4 mt-4">
                         <h2 className="text-lg font-bold mb-2">6. How Long Do We Keep Your Information?</h2>
                         <p><strong>In Short:</strong> We keep your information for as long as necessary to fulfill the purposes outlined in this privacy notice unless otherwise required by law.</p>
                         <p>We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, accounting, or other legal requirements). No purpose in this notice will require us keeping your personal information for longer than ÿ the period of time in which users have an account with us.</p>
                         <p>When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymize such information, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.</p>
-                    </div>
+                </div>
+
                 {/* How Do We Keep Your Information Safe? */}
                 <div className="border-t-2 border-gray-300 pt-4 mt-4">
                         <h2 className="text-lg font-bold mb-2">7. How Do We Keep Your Information Safe?</h2>
                         <p><strong>In Short:</strong> We aim to protect your personal information through a system of organizational and technical security measures.</p>
                         <p>We have implemented appropriate and reasonable technical and organizational security measures designed to protect the security of any personal information we process. However, despite our safeguards and efforts to secure your information, no electronic transmission over the Internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorized third parties will not be able to defeat our security and improperly collect, access, steal, or modify your information. Although we will do our best to protect your personal information, transmission of personal information to and from our Services is at your own risk. You should only access the Services within a secure environment.</p>
-                    </div>
+                </div>
 
                 {/* Do We Collect Information From Minors? */}
                 <div className="border-t-2 border-gray-300 pt-4 mt-4">
                         <h2 className="text-lg font-bold mb-2">8. Do We Collect Information From Minors?</h2>
                         <p><strong>In Short:</strong> We do not knowingly collect data from or market to children under 18 years of age.</p>
                         <p>We do not knowingly solicit data from or market to children under 18 years of age. By using the Services, you represent that you are at least 18 or that you are the parent or guardian of such a minor and consent to such minor dependent use of the Services. If we learn that personal information from users less than 18 years of age has been collected, we will deactivate the account and take reasonable measures to promptly delete such data from our records. If you become aware of any data we may have collected from children under age 18, please contact us at <a href="mailto:privacy@dena.energy.com" className="text-blue-500">privacy@dena.energy.com</a>.</p>
-                    </div>
+                </div>
 
                 {/* What Are Your Privacy Rights? */}
                 <div className="border-t-2 border-gray-300 pt-4 mt-4">
@@ -146,13 +150,13 @@ export default async function Privacy() {
                         Upon your request to terminate your account, we will deactivate or delete your account and information from our active databases. However, we may retain some information in our files to prevent fraud, troubleshoot problems, assist with any investigations, enforce our legal terms and/or comply with applicable legal requirements.</p>
                         <p><strong>Cookies and similar technologies:</strong> Most Web browsers are set to accept cookies by default. If you prefer, you can usually choose to set your browser to remove cookies and to reject cookies. If you choose to remove cookies or reject cookies, this could affect certain features or services of our Services.</p>
                         <p>If you have questions or comments about your privacy rights, you may email us at privacy@dena.energy.</p>
-                   </div>
+                </div>
 
                 {/* Controls for Do-Not-Track Features */}
                 <div className="border-t-2 border-gray-300 pt-4 mt-4">
                         <h2 className="text-lg font-bold mb-2">10. Controls for Do-Not-Track Features</h2>
                         <p>Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track ("DNT") feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. At this stage, no uniform technology standard for recognizing and implementing DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy notice.</p>
-                   </div>
+                </div>
 
                 {/* Do United States Residents Have Specific Privacy Rights? */}
                 <div className="border-t-2 border-gray-300 pt-4 mt-4">
@@ -211,7 +215,7 @@ export default async function Privacy() {
                                 <p className="pb-2">L. NO</p>
                                 </div>
                         </div>
-                  </div>
+                        </div>       
                 </div>
                 <div className="border-t-2 border-gray-300 pt-4 mt-4">
                         <p>We may also collect other personal information outside of these categories through instances where you interact with us in person, online, or by phone or mail in the context of:</p>
@@ -353,7 +357,6 @@ export default async function Privacy() {
                         <address>
                                 Dena Energy Inc<br />
                                 99 S Almaden Blvd,<br />
-                                San Jose, CA 95113, USA<br />
                                 San Jose, CA 95113<br />
                                 United States
                         </address>
